0ae7914295c6fbde37e023a721ba3a81fda20a85
Bunyod <zaripovbunyod571@gmail.com>
2026-03-29 16:26:51 +0500

    fixed

--- service/api.ts
+++ service/api.ts
@@ -1,15 +1,5 @@
 import axios from "axios";
-import { getCookie } from "cookies-next";
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-});
-
-api.interceptors.request.use((config) => {
-  const accessToken = getCookie("accessToken");
-  if (accessToken) {
-    config.headers.Authorization = `Bearer ${accessToken}`;
-  }
-
-  return config;
 });
